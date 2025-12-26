4ec177c7b4c41cc93e030bc7b2a6cb092b0bd979
8gaU8 <71952256+8gaU8@users.noreply.github.com>
2025-12-27 00:09:46 +0900

    add zsh_custom for コンパチ

--- init.zsh
+++ init.zsh
@@ -8,6 +8,9 @@ export COMPLETION_CACHE_DIR="${HOME}/.cache/zsh_completions"
 export ACTIVATE_CACHE_DIR="${HOME}/.cache/zsh_activations"
 export MAX_CACHE_AGE_SECONDS=86400  # 1 day
 
+# Set ZSH_CUSTOM (for compatibility. should be fixed later)
+export ZSH_CUSTOM="$(dirname $0)/custom"
+
 
 fpath+="${COMPLETION_CACHE_DIR}"
 
