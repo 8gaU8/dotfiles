cf289f078bcdbe0d127314ddfa13da413474416c
8gaU8 <71952256+8gaU8@users.noreply.github.com>
2025-12-26 22:08:23 +0900

    add custom scripts as plugin

--- init.zsh
+++ init.zsh
@@ -1,21 +1,11 @@
 
 export COMPLETION_CACHE_DIR="${HOME}/.cache/zsh_completions"
 
-# Path to Customization
-export ZSH_CUSTOM="$HOME/zsh-dotfiles/custom"
-
-export SHELDON_CONFIG_DIR="$(dirname $1)/sheldon"
-
-# import customs
-for config_file in $ZSH_CUSTOM/*.zsh; do
-  [ -f "$config_file" ] && source "$config_file"
-done
-
-# source $ZSH_CUSTOM/themes/ultima.zsh-theme
 
 
-fpath=("${COMPLETION_CACHE_DIR}" $fpath)
+fpath+="${COMPLETION_CACHE_DIR}"
 
 # autoload -Uz compinit && compinit
 
+export SHELDON_CONFIG_DIR="$(dirname $1)/sheldon"
 eval "$(sheldon source)"
