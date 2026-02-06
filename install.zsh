481eb393eeabd313e7b8891745f3ce8a621cd4c6
8gaU8 <71952256+8gaU8@users.noreply.github.com>
2026-02-06 10:37:50 +0900

    use ghostty as symbolic link

--- install.zsh
+++ install.zsh
@@ -3,3 +3,4 @@
 INSTALL_PATH="${HOME}/dotfiles"
 git clone https://github.com/8gau8/dotfiles "${INSTALL_PATH}"
 ln -s "${INSTALL_PATH}/config/mise.global.toml" "${HOME}/.config/mise/config.toml"
+ln -s "${INSTALL_PATH}/config/ghostty/config" "${HOME}/.config/ghostty/config"
