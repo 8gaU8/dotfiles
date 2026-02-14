e4814a9f79b0647a4c2153f21570735cdb2eb50c
8gaU8 <71952256+8gaU8@users.noreply.github.com>
2026-02-14 18:51:40 +0900

    remove ghcup related setting

--- custom/40-path.zsh
+++ custom/40-path.zsh
@@ -8,7 +8,3 @@ add_to_path_if_exists "${HOME}/.lmstudio/bin"
 add_to_path_if_exists "${HOME}/.antigravity/antigravity/bin"
 ## Rust
 add_to_path_if_exists "${HOME}/.cargo/bin"
-## ghcup
-add_to_path_if_exists "${HOME}/.ghcup/bin"
-## Cabal
-add_to_path_if_exists "${HOME}/.cabal/bin"
