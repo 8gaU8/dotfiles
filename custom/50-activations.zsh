16a14995d9717a3bb6952f5ebc6d5f77a9f12df5
8gaU8 <71952256+8gaU8@users.noreply.github.com>
2026-02-26 22:43:08 -0500

    use try-go

--- custom/50-activations.zsh
+++ custom/50-activations.zsh
@@ -1,6 +1,6 @@
 #! /usr/bin/env zsh
 
 _evalcache /opt/homebrew/bin/brew shellenv
-_evalcache ruby ~/.local/try.rb init ~/src/tries
+_evalcache try init ~/src/tries
 _evalcache fzf --zsh
 _evalcache zoxide init zsh --cmd cd
