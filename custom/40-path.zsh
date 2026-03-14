434696d1f8108f677cff47505eb157743d163303
8gaU8 <71952256+8gaU8@users.noreply.github.com>
2026-03-14 00:54:35 -0400

    simplify path addition

--- custom/40-path.zsh
+++ custom/40-path.zsh
@@ -1,8 +1,7 @@
 #! /usr/bin/env zsh
 
-## Add common user bin directories
-add_to_path_if_exists "${HOME}/.local/bin"
-## Antigravity
-add_to_path_if_exists "${HOME}/.antigravity/antigravity/bin"
-## Rust
-add_to_path_if_exists "${HOME}/.cargo/bin"
+path+=(
+  "${HOME}/.local/bin"
+  "${HOME}/.antigravity/antigravity/bin"
+  "${HOME}/.cargo/bin"
+)
