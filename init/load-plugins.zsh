2ae820ebdfee678f7624c2d7e8a94a9151f7f1f9
8gaU8 <71952256+8gaU8@users.noreply.github.com>
2026-03-14 23:55:26 -0400

    plugin 並べ替え

--- init/load-plugins.zsh
+++ init/load-plugins.zsh
@@ -2,7 +2,6 @@
 
 DOTFILES="${HOME}/dotfiles"
 
-plugin_file="${DOTFILES}/custom/00-source-plugins.gen.zsh"
 plugin_bundle_file="${DOTFILES}/custom/01-bundled-plugins.gen.zsh"
 plugins_download_dir="${DOTFILES}/plugins"
 
@@ -26,7 +25,9 @@ function source_plugin(){
     local script_to_use="$2"
     local plugin_path="${plugins_download_dir}/${plugin_name}"
     clone "${plugin_name}" "${plugin_path}"
-    echo "source ${plugin_path}/${script_to_use}" >> "$plugin_file"
+    echo "# --- Source: ${plugin_name} (${script_to_use}) ---" >> "$plugin_bundle_file"
+    echo "source ${plugin_path}/${script_to_use}" >> "$plugin_bundle_file"
+    echo -e "\n" >> "$plugin_bundle_file"
 }
 
 function bundle_plugin(){
@@ -34,14 +35,17 @@ function bundle_plugin(){
     local script_to_use="$2"
     local plugin_path="${plugins_download_dir}/${plugin_name}"
     clone "${plugin_name}" "${plugin_path}"
+    echo "# --- Source: ${plugin_name} (${script_to_use}) ---" >> "$plugin_bundle_file"
     cat "${plugin_path}/${script_to_use}" >> "$plugin_bundle_file"
     echo -e "\n" >> "$plugin_bundle_file"
 }
 
-rm -f "$plugin_file" "$plugin_bundle_file"
+rm -f "$plugin_bundle_file"
 
 source_plugin "zdharma-continuum/fast-syntax-highlighting" "fast-syntax-highlighting.plugin.zsh"
 
 bundle_plugin "zsh-users/zsh-autosuggestions" "zsh-autosuggestions.zsh"
 bundle_plugin "rcmdnk/shell-logger" "etc/shell-logger"
 bundle_plugin "ohmyzsh/ohmyzsh" "plugins/brew/brew.plugin.zsh"
+
+source_plugin "marlonrichert/zsh-autocomplete" "zsh-autocomplete.plugin.zsh"
