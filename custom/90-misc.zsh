26c11dcc10398f0222f4ee6b91dbdceea2bf6b48
8gaU8 <71952256+8gaU8@users.noreply.github.com>
2026-03-18 23:58:55 -0400

    add vim

--- custom/90-misc.zsh
+++ custom/90-misc.zsh
@@ -5,3 +5,6 @@ export PIP_REQUIRE_VIRTUALENV=1
 
 # man: use bat as manpager
 export MANPAGER="sh -c 'awk '\''{ gsub(/\x1B\[[0-9;]*m/, \"\", \$0); gsub(/.\x08/, \"\", \$0); print }'\'' | $(which bat) -p -lman'"
+
+# Vim
+export VIMINIT="source ${DOTFILES_DIR}/config/vim/.vimrc"
