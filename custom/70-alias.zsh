b35010e8c09314dd12deded85eebd1dd735f3586
8gaU8 <71952256+8gaU8@users.noreply.github.com>
2026-03-18 23:47:53 -0400

    no need for updating cmd

--- custom/70-alias.zsh
+++ custom/70-alias.zsh
@@ -13,5 +13,3 @@ alias ...='cd ./../..'
 alias history-stat="history 0 | awk '{print \$2}' | sort | uniq -c | sort -n -r | head"
 alias http-serve='python3 -m http.server'
 alias vdiff="code --new-window --diff --wait"
-
-alias u="update_zsh_custom"
