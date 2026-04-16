24f3cbdc8748eeed687cff1a65a142e34c8b4cbf
8gaU8 <71952256+8gaU8@users.noreply.github.com>
2026-04-17 01:34:01 +0900

    fix bug

--- config/karabiner/rules/externalKeyboards.ts
+++ config/karabiner/rules/externalKeyboards.ts
@@ -4,8 +4,11 @@ export function thePurpleKeyboard() {
   return rule("Device Specific Rule") // 2 rules
     .manipulators([
       withCondition(ifDevice({ vendor_id: 6700, product_id: 39689 }))([
+        // "optionalAny" is needed to make this key work as tilde when pressed with modifiers.
+        map("escape", "optionalAny")
+          .to("grave_accent_and_tilde")
+          .description("Escape to Grave Accent/Tilde"),
         map("application").to("right_option"),
-        map("escape").to("grave_accent_and_tilde"),
         map("left_control").to("fn"),
         map("right_control").to("right_option"),
         map("right_option").to("right_command"),
