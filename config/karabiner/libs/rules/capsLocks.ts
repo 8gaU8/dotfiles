0d9168579180c4e8aa6cd2adf6efc416933c2f59
8gaU8 <71952256+8gaU8@users.noreply.github.com>
2026-04-21 02:37:19 +0900

    preserve default params

--- config/karabiner/libs/rules/capsLocks.ts
+++ config/karabiner/libs/rules/capsLocks.ts
@@ -1,10 +1,11 @@
-import { map, type RuleBuilder, rule } from "karabiner.ts";
+import { map, type RuleBuilder, rule, ModifierParam } from "karabiner.ts";
 
 export function capsLockToControl(): RuleBuilder {
   const description = "Caps Lock to Left Control when held, Escape when tapped";
   return rule("Left Control → Control/Escape").manipulators([
     map("caps_lock", "optionalAny")
-      .to("left_control", {}, { lazy: true })
+      // ModifierParam should be "undefiend" to preserve the default parameters (just in case)
+      .to("left_control", undefined, { lazy: true })
       .toIfAlone("escape")
       .toIfHeldDown("left_control")
       .description(description),
