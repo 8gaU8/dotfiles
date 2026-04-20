b46ba4d177e8964ccb16d7084000d3d6b5bd47ef
8gaU8 <71952256+8gaU8@users.noreply.github.com>
2026-04-21 02:37:57 +0900

    remove unused import

--- config/karabiner/libs/rules/capsLocks.ts
+++ config/karabiner/libs/rules/capsLocks.ts
@@ -1,4 +1,4 @@
-import { map, type RuleBuilder, rule, ModifierParam } from "karabiner.ts";
+import { map, type RuleBuilder, rule } from "karabiner.ts";
 
 export function capsLockToControl(): RuleBuilder {
   const description = "Caps Lock to Left Control when held, Escape when tapped";
